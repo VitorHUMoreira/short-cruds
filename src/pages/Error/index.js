07d1dbfa59012b988e3001711f463e0d5a1cf70c
VitorHUMoreira <vitorhugo_187@hotmail.com>
2022-09-12 16:29:30 -0300

    trying footer

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,7 +1,23 @@
+import Alert from "react-bootstrap/Alert";
+import img404 from "../../assets/404.svg";
+import { Link } from "react-router-dom";
+
 function Error() {
   return (
-    <div>
-      <h1>Error</h1>
+    <div className="container-xxl">
+      <Alert className="teste" variant="danger">
+        <img src={img404} width={250} alt="404" />
+        <Alert.Heading>Opa, página não encontrada !!!</Alert.Heading>
+        <br />
+        <p>
+          Parece que você tentou acessar uma página que não existe em nosso
+          domínio.
+        </p>
+        <hr />
+        <Link to="/">
+          <button className="button">MENU</button>
+        </Link>
+      </Alert>
     </div>
   );
 }
