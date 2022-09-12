24879e682d2b5e406759a98e378f51a069cc7f3a
VitorHUMoreira <vitorhugo_187@hotmail.com>
2022-09-12 10:13:53 -0300

    starting home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
